d7f808c615a74235968c5b5b178605601f1f2292
“Anton <lllobanov.a.i@gmail.com>
2017-05-25 13:10:22 +0300

    Сделал переключения между экранами

---
+++ js/main.js
@@ -0,0 +1,44 @@
+﻿window.main = function () {
+  const _mainNode = document.querySelector(`.main`);
+  const _templatesNodeList = document.querySelector(`#templates`).content.children;
+  const _screenIndexes = [5, 0, 1, 4, 3, 2];
+
+  let _currentScreen = 0;
+
+  function keyPressHandler(evt) {
+    const KEY_LEFT = 37;
+    const KEY_RIGHT = 39;
+    if (evt.altKey) {
+      if (evt.keyCode === KEY_LEFT) {
+        showScreen(_currentScreen - 1);
+      } else if (evt.keyCode === KEY_RIGHT) {
+        showScreen(_currentScreen + 1);
+      }
+    }
+  }
+
+  function clearMainNode() {
+    while (_mainNode.lastChild) {
+      _mainNode.removeChild(_mainNode.lastChild);
+    }
+  }
+
+  function showScreen(index = 0) {
+    if (index > _templatesNodeList.length - 1 || index > _screenIndexes.length || index < 0) {
+      return;
+    }
+
+    _currentScreen = index;
+
+    clearMainNode();
+
+    _mainNode.appendChild(_templatesNodeList[_screenIndexes[index]].cloneNode(true));
+  }
+
+  function init() {
+    showScreen(_currentScreen);
+    document.addEventListener(`keyup`, keyPressHandler);
+  }
+
+  init();
+}();
