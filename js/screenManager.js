7d9e89f978660bcde3b6413c377ee798fa35dacb
Anton Lobanov <lllobanov.a.i@gmail.com>
2017-06-01 17:14:50 +0300

    #4 Тесные связи

---
+++ js/screenManager.js
@@ -0,0 +1,14 @@
+﻿const mainNode = document.querySelector(`.main`);
+
+export function clearScreen() {
+  mainNode.innerHTML = ``;
+}
+
+export function showScreen(screen) {
+  if (!screen) {
+    return;
+  }
+
+  clearScreen();
+  mainNode.appendChild(screen);
+}
