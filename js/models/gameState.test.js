5e1d4b2ceded12ef1d09498b73ebd9c19b77e56a
Anton Lobanov <lllobanov.a.i@gmail.com>
2017-06-19 20:53:25 +0300

    #7 Доверяй, но проверяй

--- js/models/gameState.test.js
+++ js/models/gameState.test.js
@@ -1,12 +1,27 @@
 import assert from 'assert';
 import {gameInitState, setLives, setAnswers, setTime} from './gameState.js';
 
-mocha.setup(`tdd`);
-
 suite(`Set lives`, () => {
-  test(`Copy on write`, () => {
+  test(`Copy on write principle`, () => {
     const gameState = Object.assign({}, gameInitState);
     assert(gameState !== setLives(gameState, 0));
+  });
+
+  test(`The value is greater than or equal to zero`, () => {
+    let gameState = Object.assign({lives: 1}, gameInitState);
+
+    gameState = setLives(gameState, 2);
+    assert(gameState.lives === 2);
+
+    gameState = setLives(gameState, 0);
+    assert(gameState.lives === 0);
+  });
+
+  test(`Only positive values`, () => {
+    let gameState = Object.assign({}, gameInitState);
+
+    gameState = setLives(gameState, -1);
+    assert(gameState.lives >= 0);
   });
 });
 
@@ -15,11 +30,45 @@ suite(`Set answers`, () => {
     const gameState = Object.assign({}, gameInitState);
     assert(gameState !== setAnswers(gameState, 0));
   });
+
+  test(`The value is greater than or equal to zero`, () => {
+    let gameState = Object.assign({answers: 10}, gameInitState);
+
+    gameState = setAnswers(gameState, 2);
+    assert(gameState.answers === 2);
+
+    gameState = setAnswers(gameState, 0);
+    assert(gameState.answers === 0);
+  });
+
+  test(`Only positive values`, () => {
+    let gameState = Object.assign({}, gameInitState);
+
+    gameState = setAnswers(gameState, -1);
+    assert(gameState.answers >= 0);
+  });
 });
 
 suite(`Set time`, () => {
   test(`Copy on write principle`, () => {
     const gameState = Object.assign({}, gameInitState);
     assert(gameState !== setTime(gameState, 0));
+  });
+
+  test(`The value is greater than or equal to zero`, () => {
+    let gameState = Object.assign({time: 10}, gameInitState);
+
+    gameState = setTime(gameState, 2);
+    assert(gameState.time === 2);
+
+    gameState = setTime(gameState, 0);
+    assert(gameState.time === 0);
+  });
+
+  test(`Only positive values`, () => {
+    let gameState = Object.assign({}, gameInitState);
+
+    gameState = setTime(gameState, -1);
+    assert(gameState.time >= 0);
   });
 });
