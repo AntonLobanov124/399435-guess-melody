45df1c534c9a47d446c7779f7c86769ca9f238d7
Anton Lobanov <lllobanov.a.i@gmail.com>
2017-06-26 17:39:37 +0300

    Добавил функцию preloadAudio для предзагрузки треков.

--- js/utils.js
+++ js/utils.js
@@ -21,3 +21,36 @@ export function arrayShuffle(array) {
 export function padLeft(value = 0) {
   return value > 9 ? value.toString() : `0${value}`;
 }
+
+export function preloadAudio(urls = []) {
+  if (!Array.isArray(urls)) {
+    throw new Error(`Invalid parameter.`);
+  }
+
+  urls = urls.filter((value, index, self) => value != `` && self.indexOf(value) === index);
+
+  let loaded = 0;
+  return new Promise((resolve, reject) => {
+    const loadNextAudio = () => {
+      loadAudio(urls[loaded]);
+    };
+
+    const loadedAudio = () => {
+      loaded++;
+      if (loaded === urls.length) {
+        resolve(loaded);
+      } else {
+        loadNextAudio();
+      }
+    };
+
+    const loadAudio = (url) => {
+      const audio = new Audio();
+
+      audio.addEventListener(`canplaythrough`, loadedAudio, false);
+      audio.src = url;
+    };
+
+    loadNextAudio();
+  });
+}
