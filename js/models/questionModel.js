fa44463fc508a4c552aa7413850942a41252cbfb
Anton Lobanov <lllobanov.a.i@gmail.com>
2017-06-27 21:03:52 +0300

    Изменил ссылку на BE.

--- js/models/questionModel.js
+++ js/models/questionModel.js
@@ -2,6 +2,6 @@ import BaseModel from '../models/baseModel.js';
 
 export default new class extends BaseModel {
   get urlRead() {
-    return `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/questions`;
+    return `https://intensive-ecmascript-server-wbcouextsi.now.sh/guess-melody/questions`;
   }
 }();
--- js/models/statisticModel.js
+++ js/models/statisticModel.js
@@ -2,10 +2,10 @@ import BaseModel from '../models/baseModel.js';
 
 export default new class extends BaseModel {
   get urlRead() {
-    return `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/stats/id39943`;
+    return `https://intensive-ecmascript-server-wbcouextsi.now.sh/guess-melody/stats/id39943`;
   }
 
   get urlWrite() {
-    return `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/stats/id39943`;
+    return `https://intensive-ecmascript-server-wbcouextsi.now.sh/guess-melody/stats/id39943`;
   }
 }();
