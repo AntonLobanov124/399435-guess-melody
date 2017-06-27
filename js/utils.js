d4c5c2452058906643de29eb72eb4b5530e16b49
Anton Lobanov <lllobanov.a.i@gmail.com>
2017-06-27 21:04:55 +0300

    Убрал не используемые методы.

--- js/utils.js
+++ js/utils.js
@@ -4,20 +4,6 @@ export function getElementFromTemplate(html) {
   return template.firstChild;
 }
 
-export function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-export function arrayShuffle(array) {
-  const ar = array.slice(0);
-  for (let i = ar.length; i; i--) {
-    let j = getRandomInt(0, i);
-    [ar[i - 1], ar[j]] = [ar[j], ar[i - 1]];
-  }
-
-  return ar;
-}
-
 export function padLeft(value = 0) {
   return value > 9 ? value.toString() : `0${value}`;
 }
