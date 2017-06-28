4212d1a0daf7da1062400c63187fc9b7993c5508
Anton Lobanov <lllobanov.a.i@gmail.com>
2017-06-29 00:45:39 +0300

    Добавил метод getMarkup.

--- js/views/abstractView.js
+++ js/views/abstractView.js
@@ -3,12 +3,12 @@ import {changeView} from '../viewManager';
 
 export default class AbstractView {
   get template() {
-    throw new Error(`Abstract method called`);
+    throw new Error(`Abstract method called.`);
   }
 
   get element() {
     if (!this._element) {
-      this._element = this._render();
+      this._element = this._getMarkup();
       this.bind();
     }
     return this._element;
@@ -16,6 +16,10 @@ export default class AbstractView {
 
   _render() {
     return getElementFromTemplate(this.template);
+  }
+
+  _getMarkup() {
+    return this._render();
   }
 
   bind() {
